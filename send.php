2a3c3877ff1f7da07833d1cbb1426bb5e803a715
DESKTOP-B8N7OS7\yutan <yuta.nozawa2018@gmail.com>
2018-10-15 02:43:20 +0900

    ファイル名変更に伴ってactionに指定した文字列も変更

--- send.php
+++ send.php
@@ -8,7 +8,7 @@
     <body>
         <h1>ひとこと掲示板</h1>
         
-        <form action="bbs.php" method="post">
+        <form action="post.php" method="post">
             名前:<input type="text" name="name" /><br />
             ひとこと:<input type="text" name="oneline" size="60" /><br />
             <input type="submit" name="submit" value="送信" />
