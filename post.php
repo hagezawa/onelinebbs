a2c37d4f64c12f20e680affeb7041f8845e31266
DESKTOP-B8N7OS7\yutan <yuta.nozawa2018@gmail.com>
2018-10-15 02:42:48 +0900

    データベースの接続とバリデーションチェックの処理を追加
    ・変数に文字列を与えて、接続できるようにした
    ・接続に関してエラーが発生したら例外処理を投げる
    ・入力されたフォームの内容に、未入力、空文字があった場合はエラーメッセージを投げる
    
    10/15やること
    ・SQLインジェクショ対策
    ・INSERT文を書く

--- post.php
+++ post.php
@@ -1,12 +1,53 @@
 <?php
 
+    $dsn = "mysql:host=localhost;dbname=oneline_bbs";
+    $user = 'root';
+    $password = '';
     //データベースに接続
-    $db = new PDO('mysql:host=localhost;dbname=oneline_bbs;charset=utf8', 'root', '');
-    
-    $result = $db->query("select * from oneline_bbs.post")
-    ->fetch(PDO::FETCH_NUM);
 
-    print_r($result);
+    try {
+        $dbh = new PDO($dsn, $user, $password);
+        
+        //エラーの属性を設定、PDO::ERRMODE_EXCEPTIONで例外を投げる
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    } catch (PDOException $e) {
+        echo 'Connection failed' . $e->getMessage();
+    }
+
+    $errors = array();
+
+    //POSTなら保存処理実行
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $name = null;
+
+        //バリデーションエラーがないか判定する
+        if (!isset($_POST['name']) || !strlen($_POST['name'])) {
+            $errors['name'] = '名前を入力してください';
+        } elseif (strlen($_POST['name'] > 40)) {
+            $errors['name'] = '名前は40文字以内で入力して下さい';
+        } else {
+            $name = $_POST['name'];
+        }
+
+        //ひとことが正しく入力されているかチェック
+        $comment = null;
+
+        if (!isset($_POST['name']) || !strlen($_POST['name'])) {
+            $errors['comment'] = 'ひとことを入力して下さい';
+        } elseif (strlen($_POST['name'] > 200)) {
+            $errors['comment'] = 'ひとことは200文字以内で入力して下さい';
+        } else {
+            $comment = $_POST['comment'];
+        }
+
+        //エラーがなければ保存
+        if (count($errors) === 0) {
+            TODO: SQLインジェクション対策のコードを書く
+            TODO: INSERT文のコードを書く
+            
+        }
+    }
 
     $db = null;
 ?>
